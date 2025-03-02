966369d5b00c6a1d72497d62b4e002ed6ef3c351
alisonratcliffe93@gmail.com <68648399+ratcliffealison@users.noreply.github.com>
2025-03-02 13:02:50 +0100

    change repo name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "ramonratcliffe/ramonratcliffe.github.io", // Replace with your actual repo name
+  base: "/ramonratcliffe.github.io", // Replace with your actual repo name
   build: {
     outDir: "dist",
   },
